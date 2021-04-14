1728dce3944c0cd715cd1e20bc15a98124379a40
Taylan YILDIZ <taylanyildz@outlook.com>
2021-04-15 00:42:23 +0300

    update change photo

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,176 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
 import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:image_cropper/image_cropper.dart';
 
 class HomeScreen extends StatefulWidget {
   final String title;
 
-  const HomeScreen({
-    Key key,
-    this.title,
-  }) : super(key: key);
+  const HomeScreen({Key key, this.title}) : super(key: key);
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var imageFiles = <File>[];
-  bool isGallery = false;
-
-  Future _selectPhoto(index) async {
-    final file = await Utils.pickMedia(
-      isGallery: isGallery,
-      cropImage: cropSquareImage,
+  var imagesFile = <File>[];
+  Future selectPhoto(index) async {
+    final file = await UtilsImage.imagePicker(
+      sourceCode: SourceCode.gallery,
+      croppImage: croppImage,
     );
     if (file == null) return;
-    if (imageFiles.length < 3)
-      setState(() => imageFiles.add(file));
-    else
-      setState(() {
-        imageFiles.removeAt(index);
-        imageFiles.insert(index, file);
-      });
+    if (imagesFile.length == (index + 1)) {
+      imagesFile.removeAt(index);
+      setState(() => imagesFile.insert(index, file));
+    } else
+      setState(() => imagesFile.add(file));
   }
 
-  Future<File> cropSquareImage(File imageFile) async {
+  Future<File> croppImage(File imageFile) async {
     return await ImageCropper.cropImage(
       sourcePath: imageFile.path,
-      aspectRatio: CropAspectRatio(ratioX: 0.5, ratioY: .5),
-      aspectRatioPresets: [CropAspectRatioPreset.square],
-      compressQuality: 70,
+      aspectRatio: CropAspectRatio(ratioX: 1, ratioY: 1),
+      maxHeight: 100,
+      maxWidth: 100,
       compressFormat: ImageCompressFormat.png,
       androidUiSettings: androidUiSettingsLock(),
-      iosUiSettings: iosUiSettingsLock(),
-    );
-  }
-
-  IOSUiSettings iosUiSettingsLock() {
-    return IOSUiSettings(
-      aspectRatioLockEnabled: true,
-      rotateButtonsHidden: false,
-      rectX: 1.0,
-      rectY: 1.0,
-      rotateClockwiseButtonHidden: false,
+      iosUiSettings: iosUiSettings(),
     );
   }
 
   AndroidUiSettings androidUiSettingsLock() {
     return AndroidUiSettings(
-      toolbarColor: Colors.white,
-      toolbarTitle: 'Cropp Image',
-      toolbarWidgetColor: Colors.red,
+      lockAspectRatio: false,
+      toolbarTitle: 'Image Cropper',
+      toolbarColor: Colors.orange,
+      toolbarWidgetColor: Colors.white,
+      activeControlsWidgetColor: Colors.white,
       hideBottomControls: true,
-      cropFrameColor: Colors.red,
-      activeControlsWidgetColor: Colors.red,
-      lockAspectRatio: true,
     );
   }
 
-  _displayPhoto(context, index) {
-    if (imageFiles.isEmpty) return _imageContainer(context, index);
-    if (imageFiles[0] != null) {
-      return index == 0
-          ? _imageContainer(context, index, images: imageFiles[0])
-          : index == 1 && imageFiles.length > 1 && imageFiles[1] != null
-              ? _imageContainer(context, index, images: imageFiles[1])
-              : index == 2 && imageFiles.length > 2 && imageFiles[2] != null
-                  ? _imageContainer(context, index, images: imageFiles[2])
-                  : _imageContainer(context, index);
-    }
+  IOSUiSettings iosUiSettings() {
+    return IOSUiSettings(
+      minimumAspectRatio: 1.0,
+    );
   }
 
-  _imageContainer(context, index, {File images}) {
+  _controllerImageFile(contex, index) {
+    if (imagesFile.isEmpty) return displayImageFile(context, index);
+    return index == 0
+        ? displayImageFile(context, index, img: imagesFile[0])
+        : index == 1 && imagesFile.length > 1 && imagesFile[1] != null
+            ? displayImageFile(context, index, img: imagesFile[1])
+            : index == 2 && imagesFile.length > 2 && imagesFile[2] != null
+                ? displayImageFile(context, index, img: imagesFile[2])
+                : displayImageFile(context, index);
+  }
+
+  displayImageFile(context, index, {img}) {
+    final child = img == null
+        ? Icon(
+            Icons.image,
+            color: Colors.red,
+            size: 35.0,
+          )
+        : Image.file(
+            img,
+            fit: BoxFit.cover,
+          );
     return Stack(
       children: [
-        images == null
-            ? Container(
-                margin: EdgeInsets.all(5.0),
-                width: 120,
-                height: double.infinity,
-                color: Colors.white,
-                child: images != null
-                    ? Image.file(
-                        images,
-                        fit: BoxFit.cover,
-                      )
-                    : Icon(
-                        Icons.image,
-                        color: Colors.red,
-                      ),
-              )
-            : Container(
-                margin: EdgeInsets.all(5.0),
-                width: 120,
-                height: double.infinity,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                child: images != null
-                    ? ClipRRect(
-                        borderRadius: BorderRadius.circular(20.0),
-                        child: Image.file(
-                          images,
-                          fit: BoxFit.cover,
-                        ),
-                      )
-                    : Icon(
-                        Icons.image,
-                        color: Colors.red,
-                      ),
-              ),
+        Container(
+          margin: EdgeInsets.all(5.0),
+          width: 100.0,
+          height: double.infinity,
+          color: Colors.blue.withOpacity(.6),
+          child: child,
+        ),
         Positioned(
-          bottom: 0.0,
           right: 0.0,
+          bottom: 0.0,
           child: GestureDetector(
-            onTap: () async {
-              await showCupertinoModalPopup(
-                context: context,
-                builder: (context) => CupertinoActionSheet(
-                    actions: [
-                      CupertinoActionSheetAction(
-                        onPressed: () {
-                          isGallery = true;
-                          Navigator.pop(context);
-                          _selectPhoto(index);
-                        },
-                        child: Text('Select a Photo'),
-                        isDestructiveAction: true,
-                      ),
-                      CupertinoActionSheetAction(
-                        onPressed: () {
-                          isGallery = false;
-                          Navigator.pop(context);
-                          _selectPhoto(index);
-                        },
-                        child: Text('Take a Photo'),
-                        isDestructiveAction: false,
-                      ),
-                    ],
-                    cancelButton: CupertinoActionSheetAction(
-                      child: Text('Çık'),
-                      onPressed: () => Navigator.pop(context),
-                    )),
-              );
-            },
+            onTap: () => selectPhoto(index),
             child: Container(
-              padding: EdgeInsets.all(5.0),
+              padding: const EdgeInsets.all(5.0),
               decoration: BoxDecoration(
-                color: Colors.blue,
                 shape: BoxShape.circle,
+                color: Colors.blue,
               ),
-              child: Center(
-                child: Icon(
-                  images == null ? Icons.add : Icons.edit,
-                  size: 20.0,
-                  color: Colors.white,
-                ),
+              child: Icon(
+                Icons.add,
+                color: Colors.white,
               ),
             ),
           ),
-        )
+        ),
       ],
     );
   }
@@ -178,51 +112,19 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Stack(
-        alignment: Alignment.bottomCenter,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                margin: EdgeInsets.fromLTRB(20.0, 200.0, 20.0, 40.0),
-                child: Text(
-                  'Select Profile Photo',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Container(
-                height: 150.0,
-                width: MediaQuery.of(context).size.width,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: 3,
-                  itemBuilder: (context, index) =>
-                      _displayPhoto(context, index),
-                ),
-              ),
-            ],
-          ),
-          Positioned(
-            bottom: 40.0,
-            child: SizedBox(
-              width: 200.0,
-              child: MaterialButton(
-                onPressed: () => print('selected'),
-                color: Colors.red,
-                child: Text(
-                  'Contiune',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 150.0,
+            color: Colors.orange,
+            child: ListView.builder(
+              physics: NeverScrollableScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              itemCount: 3,
+              itemBuilder: (context, index) =>
+                  _controllerImageFile(context, index),
             ),
           )
         ],
@@ -231,19 +133,24 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class Utils {
-  static Future<File> pickMedia({
-    @required bool isGallery,
-    Future<File> Function(File file) cropImage,
+enum SourceCode {
+  gallery,
+  camera,
+}
+
+class UtilsImage {
+  static Future<File> imagePicker({
+    @required SourceCode sourceCode,
+    @required Future<File> Function(File file) croppImage,
   }) async {
-    final source = isGallery ? ImageSource.gallery : ImageSource.camera;
-    final pickedFile = await ImagePicker().getImage(source: source);
-    if (pickedFile == null) return null;
-    if (cropImage == null) {
-      return File(pickedFile.path);
-    } else {
-      final file = File(pickedFile.path);
-      return cropImage(file);
-    }
+    final source = sourceCode == SourceCode.gallery
+        ? ImageSource.gallery
+        : ImageSource.camera;
+    final pickerImage = await ImagePicker().getImage(source: source);
+    if (pickerImage == null) return null;
+    if (croppImage == null)
+      return File(pickerImage.path);
+    else
+      return croppImage(File(pickerImage.path));
   }
 }
